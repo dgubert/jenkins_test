727befa1234ed0b744a6b26c53e2a364e2ca8a36
Test DG User <Test DG Email>
2024-10-19 23:52:26 +0300

    added test for jenkins

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/RegistrationPageTests.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import static io.qameta.allure.Allure.step;
+import static utils.RandomUtils.*;
+
+public class RegistrationPageTests extends BaseTest {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void successfulRegistrationTest() {
+        String firstName = getFirstName(),
+                lastName = getLastName(),
+                email = getEmail(),
+                gender = getGender(),
+                number = getNumber(),
+                dayOfBirth = getDayOfBirthDate(),
+                monthOfBirth = getMonthOfBirthDate(),
+                yearOfBirth = getYearOfBirthDate(),
+                subject = getSubject(),
+                hobby = getHobby(),
+                picture = getPicture(),
+                address = getAddress(),
+                state = getState(),
+                city = getCity(state);
+
+        step("Открываем страницу формы", () -> {
+            registrationPage.openPage();
+        });
+
+        step("Заполняем форму данными и сохраняем", () -> {
+            registrationPage.setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .selectGender(gender)
+                .setUserNumber(number)
+                .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setSubject(subject)
+                .setHobby(hobby)
+                .setPicture(picture)
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .submitForm();
+        });
+
+        step("Проверяем данные в таблице", () -> {
+            registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", number)
+                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobby)
+                .checkResult("Picture", picture)
+                .checkResult("Address", address)
+                .checkResult("State and City", state + " " + city);
+        });
+    }
+}
