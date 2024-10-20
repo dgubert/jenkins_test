f8cdd22bc0e1b3ee9de69e25d0f3ed2d5be83fcc
Denis <dgubert86@gmail.com>
2024-10-21 00:29:35 +0300

    added params for test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -13,13 +13,14 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class BaseTest {
     @BeforeAll
     static void setUp() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
+        Configuration.browser = System.getProperty("browserName", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         //Configuration.holdBrowserOpen = true;
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoidUrl", "selenoid.autotests.cloud") + "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
